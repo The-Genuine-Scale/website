689c6f5bc86228932b4b79d2db24eae3263ab80f
photon0205 <sp.code2003@gmail.com>
2023-07-07 02:00:31 +0530

    feat: add style for account page

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { getUserDetails, updateUserDetails } from "../../api/user";
 import { useNavigate } from "react-router-dom";
 import "./Account.css";
+import Stats from "../../components/Stats/Stats";
 
 const Account = () => {
   const [userDetails, setUserDetails] = useState({});
-  const [isEditMode, setIsEditMode] = useState(false);
   const [newAddress, setNewAddress] = useState("");
   const navigate = useNavigate();
   const userId = localStorage.getItem("uid");
@@ -34,7 +34,6 @@ const Account = () => {
   const handleSaveChanges = async () => {
     try {
       await updateUserDetails(userId, userDetails);
-      setIsEditMode(false);
       console.log("User details updated successfully!");
     } catch (error) {
       console.log(error);
@@ -69,20 +68,12 @@ const Account = () => {
   };
 
   return (
+    <>
     <div className="account-page">
     <div className="label">
       <h1>Account Details</h1>
       <div>
-        <div className="edit">
-          {!isEditMode && (
-            <button onClick={() => setIsEditMode(true)}>Edit</button>
-          )}
-          {isEditMode && (
-            <button onClick={handleSaveChanges}>Save Changes</button>
-          )}
-        </div>
           Addresses:
-          {isEditMode ? (
             <div >
               {userDetails.address &&
                 userDetails.address.map((address) => (
@@ -100,21 +91,6 @@ const Account = () => {
               />
               <button onClick={handleAddAddress}>Add Address</button>
             </div>
-          ) : (
-            <div>
-              {userDetails.address &&
-                userDetails.address.map((address) => (
-                  <div key={address}>
-                    {address}
-                    {isEditMode && (
-                      <button onClick={() => handleDeleteAddress(address)}>
-                        Delete
-                      </button>
-                    )}
-                  </div>
-                ))}
-            </div>
-          )}
       </div>
       </div>
       <div className="label">
@@ -123,7 +99,6 @@ const Account = () => {
             type="text"
             name="dob"
             value={userDetails.dob || ""}
-            disabled={!isEditMode}
             onChange={handleInputChange}
           />
       </div>
@@ -133,7 +108,6 @@ const Account = () => {
             type="email"
             name="email"
             value={userDetails.email || ""}
-            disabled={!isEditMode}
             onChange={handleInputChange}
           />
       </div>
@@ -143,7 +117,6 @@ const Account = () => {
             type="text"
             name="gender"
             value={userDetails.gender || ""}
-            disabled={!isEditMode}
             onChange={handleInputChange}
           />
       </div>
@@ -153,7 +126,6 @@ const Account = () => {
             type="text"
             name="mobileNumber"
             value={userDetails.mobileNumber || ""}
-            disabled={!isEditMode}
             onChange={handleInputChange}
           />
       </div>
@@ -163,7 +135,6 @@ const Account = () => {
             type="text"
             name="name"
             value={userDetails.name || ""}
-            disabled={!isEditMode}
             onChange={handleInputChange}
           />
       </div>
@@ -177,11 +148,14 @@ const Account = () => {
           />
       </div>
       <div className="all_buttons">
-        <button onClick={handleViewCart}>View Cart</button>
-        <button onClick={handleViewOrderHistory}>View Order History</button>
+      <button onClick={handleSaveChanges}>Save</button>
+        <button onClick={handleViewCart}>Cart</button>
+        <button onClick={handleViewOrderHistory}>Order History</button>
         {/* <button onClick={handleViewWishlist}>View Wishlist</button> */}
       </div>
     </div>
+    <Stats />
+    </>
   );
 };
 
